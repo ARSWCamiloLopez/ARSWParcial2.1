8e5ffe8f14c463888d018f44228755be4a389616
Camilo Lopez <camilino123@gmail.com>
2018-10-25 18:13:15 -0500

    Avance parcial

--- src/main/resources/static/js/ParcialController.js
+++ src/main/resources/static/js/ParcialController.js
@@ -6,40 +6,45 @@
 
 
 function getWeatherByCityName() {
-    axios.get('weather/' + document.getElementById("mainInput").value)
-            .then(function (response) {
 
-                document.getElementById("tableDiv").innerHTML = "";
+    if (document.getElementById("mainInput").value !== ""){
+        axios.get('weather/' + document.getElementById("mainInput").value)
+                .then(function (response) {
 
-                var weatherTable = document.createElement("table");
-                weatherTable.setAttribute("class", "table");
-                weatherTable.style.width = "50%";
+                    document.getElementById("tableDiv").innerHTML = "";
 
-                var tableTitles = ["Temperature", "Pressure", "Humidity", "Min Temperature", "Max Temperature"];
+                    var weatherTable = document.createElement("table");
+                    weatherTable.setAttribute("class", "table");
+                    weatherTable.style.width = "50%";
 
-                var titleRow = document.createElement("tr");
+                    var tableTitles = ["Temperature", "Pressure", "Humidity", "Min Temperature", "Max Temperature"];
 
-                for (var x in tableTitles) {
-                    var rowItem = document.createElement("th");
-                    rowItem.innerHTML = tableTitles[x];
-                    titleRow.appendChild(rowItem);
-                }
+                    var titleRow = document.createElement("tr");
 
-                weatherTable.appendChild(titleRow);
+                    for (var x in tableTitles) {
+                        var rowItem = document.createElement("th");
+                        rowItem.innerHTML = tableTitles[x];
+                        titleRow.appendChild(rowItem);
+                    }
 
-                for (var x in response.data) {
-                    if (x.includes("main")) {
-                        var infoRow = document.createElement("tr");
-                        for(var y in response.data[x]){
-                            var rowItem = document.createElement("td");
-                            rowItem.innerHTML = response.data[x][y]
-                            infoRow.appendChild(rowItem);
+                    weatherTable.appendChild(titleRow);
+
+                    for (var x in response.data) {
+                        if (x.includes("main")) {
+                            var infoRow = document.createElement("tr");
+                            for (var y in response.data[x]) {
+                                var rowItem = document.createElement("td");
+                                rowItem.innerHTML = response.data[x][y]
+                                infoRow.appendChild(rowItem);
+                            }
+                            weatherTable.appendChild(infoRow);
                         }
-                        weatherTable.appendChild(infoRow);
-                    }                    
-                }
-                       
-                document.getElementById("tableDiv").appendChild(weatherTable);
-                
-            });
+                    }
+
+                    document.getElementById("tableDiv").appendChild(weatherTable);
+
+                });
+    }else{
+        alert("Debe ingresar una Ciudad");
+    }
 }
