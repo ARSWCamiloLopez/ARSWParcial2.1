e7f61f9070e9b5691a9c99def913e721ee15cd6f
Camilo Lopez <camilino123@gmail.com>
2018-10-25 16:24:12 -0500

    Avance parcial

---
+++ src/main/java/edu/eci/arsw/parcial/controller/ParcialAPIController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.parcial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author 2105700
+ */
+@RestController
+@RequestMapping(value = "/weather")
+public class ParcialAPIController {
+    
+}
---
+++ src/main/java/edu/eci/arsw/parcial/services/ParcialServices.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.parcial.services;
+
+/**
+ *
+ * @author 2105700
+ */
+public interface ParcialServices {
+    
+}
